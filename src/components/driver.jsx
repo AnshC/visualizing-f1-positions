b185587816705652be1b14ab94e115375c9d808e
AnshC <63086888+AnshC@users.noreply.github.com>
2024-09-04 18:27:10 -0400

    null API response handling

--- src/components/driver.jsx
+++ src/components/driver.jsx
@@ -1,38 +1,55 @@
 import "./styles.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import { MdOutlinePersonOff } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 
 export default function Driver(props) {
   const [backgroundColor, setBackgroundColor] = useState("var(--f1-black)");
   const [teamTextColor, setTeamTextColor] = useState(`#${props.color}`);
+  const [mainColor, setMainColor] = useState(props.color);
+  const [altColor, setAltColor] = useState("#FFFFFF");
+
+  useEffect(() => {
+    if (props.color === null) {
+      setMainColor("FFFFFF");
+      setTeamTextColor("#FFFFFF");
+    }
+  }, [props.color]);
 
   return (
     <div
       onMouseEnter={() => {
-        setBackgroundColor(`#${props.color}`);
+        setBackgroundColor(`#${mainColor}`);
         setTeamTextColor("var(--f1-black)");
+        if (props.color === null) {
+          setAltColor("#000000");
+        }
       }}
       onMouseLeave={() => {
         setBackgroundColor("var(--f1-black)");
-        setTeamTextColor(`#${props.color}`);
+        setTeamTextColor(`#${mainColor}`);
+        if (props.color === null) {
+          setAltColor("#FFFFFF");
+        }
       }}
       className="driver"
       style={{
-        border: `solid 3px #${props.color}`,
+        border: `solid 3px #${mainColor}`,
         backgroundColor: backgroundColor,
       }}
     >
       <div className="number">
-        <h2>{props.number}</h2>
+        <h2 style={{ color: altColor }}>{props.number}</h2>
       </div>
       <div className="text">
-        <h2>{props.name}</h2>
-        <p style={{ color: teamTextColor }}>{props.team}</p>
+        <h2 style={{ color: altColor }}>{props.name}</h2>
+        <p style={{ color: teamTextColor }}>
+          {props.team === null ? "N/A" : props.team}
+        </p>
       </div>
       <div className="img">
         {props.img_url === null ? (
-          <MdOutlinePersonOff id="broken-img" />
+          <FaUser style={{ color: altColor }} id="broken-img" />
         ) : (
           <img src={props.img_url} alt={`${props.name} headshot`} />
         )}
